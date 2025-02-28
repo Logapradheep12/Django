571e439dd0d147ef2ac8cc8bd38f45ade8ef549e
Logapradheep12 <leoprasath7@gmail.com>
2025-02-28 16:00:49 +0530

    feat : create rigister and collection page

--- E-Commerce Project/shopkart/shop/views.py
+++ E-Commerce Project/shopkart/shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 
@@ -6,3 +7,6 @@ def home(request):
     return render(request,"index.html")
 def register(request):
     return render(request,"register.html")
+def collections(request):
+    catagory = Catagory.objects.filter(status=0)
+    return render(request,"collections.html",{"catagory":catagory})
